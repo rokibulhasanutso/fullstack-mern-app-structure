8f1ae85cd8377c6091dbf40659b97dbcf272213c
rokibulhasan <rokibulhasanutsoo@gmail.com>
2024-02-29 20:36:15 +0600

    initialize structure

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello Rokibul! welcome to your new project.
+    </h1>
+  );
+};
+
+export default App;
---
+++ server/src/routes/api.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+// Create routes here
+
+module.exports = router;
